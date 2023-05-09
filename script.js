f65fe59e02932dc6854619d2f0db1514c3f64870
Antonio Soto <antoniosotopwr@gmail.com>
2023-05-08 18:30:00 -0600

    added function to see the value(number) of the slider when is used it

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log("hello");
+
+const labelPassword = document.querySelector(".card__password");
+const labelLenghtPassword = document.querySelector(".card__passwordLenght");
+const slider = document.querySelector(".card__slider");
+const btnGeneratePassword = document.querySelector(".card__btn");
+
+const displayLenghtPassword = () => {
+  labelLenghtPassword.textContent = slider.value;
+};
