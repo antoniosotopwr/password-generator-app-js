0d670c449a2cf52c0b3c82de23b5733e7ebcdb26
Antonio Soto <antoniosotopwr@gmail.com>
2023-05-10 09:11:17 -0600

    addded arrays of letters,numbers and symbols and a random number generator function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-//Elements
+//--ELEMENTS
 const labelPassword = document.querySelector(".card__password");
 const labelLenghtPassword = document.querySelector(".card__passwordLenght");
 let sliderInput = document.querySelector(".card__slider");
@@ -12,9 +12,23 @@ const chkLowerInput = document.querySelector(".checkboxLower");
 const chkNumberInput = document.querySelector(".checkboxNumbers");
 const chkSymbolInput = document.querySelector(".checkboxSymbols");
 
-//Functions
-//Function to display the current value of the slider with oninput property
+//--ARRAYS
+const upperArray = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"]; //25,26
+const lowerArray = [..."abcdefghijklmnopqrstuvwxyz"]; //25,26
+const numbersArray = [..."0123456789"]; //9,10
+const symbolsArray = [..."!@#$%^&*?"]; // 8,9
+
+//--FUNCTIONS
+let password = "";
 let sliderNumber = 1;
+
+//generate a random num between 0...N-1
+const randomNum = (max) => {
+  return Math.trunc(Math.random() * max) + 1 - 1;
+};
+
+
+//Function to display the current value of the slider with oninput property
 const displayLenghtPassword = () => {
   labelLenghtPassword.textContent = sliderInput.value;
   sliderNumber = Number(sliderInput.value);
@@ -22,6 +36,7 @@ const displayLenghtPassword = () => {
   //check the passwordLenght to change square colors and border
   if (sliderNumber === 20) {
     squareLevel4.classList.add("colorLevel");
+    //change label level (low -> very high)
   } else {
     squareLevel4.classList.remove("colorLevel");
   }
