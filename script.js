d774bf2982d4e5265ad32bde22700a26f208ec34
Antonio Soto <antoniosotopwr@gmail.com>
2023-06-01 15:02:11 -0600

    added validation to checkboxes with 3 options

--- script.js
+++ script.js
@@ -46,57 +46,118 @@ const displayLenghtPassword = () => {
   }
 };
 
-//Function to check checkboxes
+//it should be from greater options to lower
+// (first the 4 option, then all the 3 posible combinations, then 2 and so on)
+
+//analice where add the none option
+// (if at first or if it could be the default case(but i guess it should be the first one))
+
+//function to check checkboxes (only checkboxes first)
 btnGeneratePassword.addEventListener("click", function () {
   if (
+    chkUpperInput.checked &&
+    chkLowerInput.checked &&
+    chkNumberInput.checked &&
+    chkSymbolInput.checked
+  ) {
+    console.log("Upper,Lower, Number and Symbol (all)");
+  }
+  //--- 3 OPTIONS ---
+  // 1 1 1 0
+  else if (
     chkUpperInput.checked &&
     chkLowerInput.checked &&
     chkNumberInput.checked
   ) {
     console.log("Upper,Lower and number");
-    // for (let i = 0; i < sliderNumber; i++) {
-    //   password = password + `${upperLowerNumberArray[randomNum(3)][randomNum(26)]}`;
-    // }
-    // console.log(password);
-    // password = "";
-    for (let i = 1; i <= sliderNumber; i++) {
-      console.log(`iteration: ${i}`);
-      for (let j = 1; i <= sliderNumber; i++) {
-        chosenArray = upperLowerNumberArray[randomNum(3)];
-        // console.log(chosenArray);
-        if (chosenArray === upperArray) {
-          letter = upperArray[randomNum(26)];
-          console.log(`Letter: ${letter}`);
-        } else if (chosenArray === lowerArray) {
-          letter = lowerArray[randomNum(26)];
-          console.log(`Letter: ${letter}`);
-        }else if(chosenArray === numbersArray){
-          letter = numbersArray[randomNum(10)];
-          console.log(`Letter: ${letter}`);
-        }
-        password = password + letter;
-      }
-    }
-    console.log(`Password:${password}, lenght:${password.length}`);
-    password = "";
-    letter = "";
-  } else if (chkUpperInput.checked && chkLowerInput.checked) {
+  }
+  // 0 1 1 1
+  else if (
+    chkLowerInput.checked &&
+    chkNumberInput.checked &&
+    chkSymbolInput.checked
+  ) {
+    console.log("Lower, number and symbol");
+  }
+  // 1 0 1 1
+  else if (
+    chkUpperInput.checked &&
+    chkNumberInput.checked &&
+    chkSymbolInput.checked
+  ) {
+    console.log("upper, number and symbol");
+  }
+  // 1 1 0 1
+  else if (
+    chkUpperInput.checked &&
+    chkLowerInput.checked &&
+    chkSymbolInput.checked
+  ) {
+    console.log("upper, lower and symbol");
+  }
+  //---2 options
+  else if (chkUpperInput.checked && chkLowerInput.checked) {
     console.log("Upper and Lower");
-    for (let i = 0; i < sliderNumber; i++) {
-      password = password + `${upperLowerArray[randomNum(2)][randomNum(26)]}`;
-    }
-    console.log(password);
-    password = "";
+  } else if (chkUpperInput.checked && chkNumberInput.checked) {
+    console.log("Upper and number");
   } else if (chkUpperInput.checked) {
     console.log("Only upper");
-    for (let i = 0; i < sliderNumber; i++) {
-      password = password + `${upperArray[randomNum(26)]}`;
-    }
-    console.log(password);
-    password = "";
+  } else {
+    console.log("Default case");
   }
 });
 
+//Function to check checkboxes (SOME WORKING-- NOT DELETE)
+// btnGeneratePassword.addEventListener("click", function () {
+//   if (
+//     chkUpperInput.checked &&
+//     chkLowerInput.checked &&
+//     chkNumberInput.checked
+//   ) {
+//     console.log("Upper,Lower and number");
+
+//     for (let i = 1; i <= sliderNumber; i++) {
+//       console.log(`iteration: ${i}`);
+//       for (let j = 1; i <= sliderNumber; i++) {
+//         chosenArray = upperLowerNumberArray[randomNum(3)];
+//         // console.log(chosenArray);
+//         if (chosenArray === upperArray) {
+//           letter = upperArray[randomNum(26)];
+//           console.log(`Letter: ${letter}`);
+//         } else if (chosenArray === lowerArray) {
+//           letter = lowerArray[randomNum(26)];
+//           console.log(`Letter: ${letter}`);
+//         } else if (chosenArray === numbersArray) {
+//           letter = numbersArray[randomNum(10)];
+//           console.log(`Letter: ${letter}`);
+//         }
+//         password = password + letter;
+//         labelPassword.textContent = password;
+//       }
+//     }
+//     console.log(`Password:${password}, lenght:${password.length}`);
+//     password = "";
+//     letter = "";
+//   } else if (chkUpperInput.checked && chkLowerInput.checked) {
+//     console.log("Upper and Lower");
+//     for (let i = 0; i < sliderNumber; i++) {
+//       password = password + `${upperLowerArray[randomNum(2)][randomNum(26)]}`;
+//     }
+//     console.log(password);
+//     labelPassword.textContent = password;
+//     password = "";
+//   } else if (chkUpperInput.checked) {
+//     console.log("Only upper");
+//     for (let i = 0; i < sliderNumber; i++) {
+//       password = password + `${upperArray[randomNum(26)]}`;
+//     }
+//     console.log(password);
+//     labelPassword.textContent = password;
+//     password = "";
+//   }
+// });
+
+//I DONT KNOW WHAT WAS THIS XD
 // for (let i = 1; i <= 6; i++) {
 //   console.log(`iteration: ${i}`);
 //   for (let j = 1; i <= 6; i++) {
@@ -118,3 +179,6 @@ btnGeneratePassword.addEventListener("click", function () {
 // console.log(`Password:${password}, lenght:${password.length}`);
 // password = "";
 // letter = "";
+
+//to do
+//se if we can create a function with both for loops for dry principle
